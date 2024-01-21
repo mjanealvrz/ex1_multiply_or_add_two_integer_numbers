2778e291568c868c61962c3e3c0341abcbec49e5
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 01:43:54 +0800

    If- else statement

--- finding_sum_or_product.py
+++ finding_sum_or_product.py
@@ -10,14 +10,21 @@ input_num2 = int(input("Enter 2nd number: "))
 
 # Create function with if-else statement
 def mutiply_or_add(input_num1,input_num2):
-    
+
     #calculate the product of two integer numbers
     product = input_num1 * input_num2
 
-
-   
     # using if-else statement define if the product is lower or equal to 1000
-    # if true, return the product
-    # if false, return the sum of input_num1 and input_num2
+    if  product <=  1000:
+
+        # if true, return the product
+        return (" The Product is " + str(product))
+    
+     # if false, return the sum of input_num1 and input_num2    
+    else: 
+        return ("The Sum is  " + str(input_num1 +  input_num2) )
+       
+
+    
 #  Call the multiply_or_add function with user-input values
 # Print result 
