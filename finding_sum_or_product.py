e1dbe10bc6f0a6ead609fee94d3c0d0768d20f57
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 01:06:16 +0800

    Created the pseudocode or the flow of the progran

--- finding_sum_or_product.py
+++ finding_sum_or_product.py
@@ -1,1 +1,14 @@
 #Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+
+
+#Pseudocode
+
+# Ask user to input two integer numbers
+# Create function with if-else statement
+    #calculate the product of two integer numbers
+    # using if-else statement define if the product is lower or equal to 1000
+    # if true, return the product
+    # if false, return the sum of input_num1 and input_num2
+#  Call the multiply_or_add function with user-input values
+# Print result 
